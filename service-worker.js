17655dc96c1e5b90fd211783cd0e710feccbb1f2
Chafik Amaziane <chafik.amaziane@gmail.com>
2025-02-06 16:35:28 -0500

    make it work offline

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = "to-do-pwa-cache-v1";
 const FILES_TO_CACHE = [
-  "/YOUR-REPOSITORY-NAME/",
-  "/YOUR-REPOSITORY-NAME /index.html",
-  "/YOUR-REPOSITORY-NAME /style.css",
-  "/YOUR-REPOSITORY-NAME /app.js",
-  "/YOUR-REPOSITORY-NAME /manifest.json",
-  "/YOUR-REPOSITORY-NAME /icons/icon-128.png",
-  "/YOUR-REPOSITORY-NAME /icons/icon-512.png",
+  "/todo-app-trends/",
+  "/todo-app-trends/index.html",
+  "/todo-app-trends/style.css",
+  "/todo-app-trends/app.js",
+  "/todo-app-trends/manifest.json",
+  "/todo-app-trends/icons/icon-128.png",
+  "/todo-app-trends/icons/icon-512.png",
 ];
 self.addEventListener("install", (event) => {
   event.waitUntil(
